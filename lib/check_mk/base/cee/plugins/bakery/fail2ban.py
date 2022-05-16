981c135c128891fe4b3e60700e6106c1300aeb3a
agabellini <andrea.gabellini@telecomitalia.sm>
2022-05-16 12:15:01 +0200

    Update fail2ban.py

--- lib/check_mk/base/cee/plugins/bakery/fail2ban.py
+++ lib/check_mk/base/cee/plugins/bakery/fail2ban.py
@@ -3,19 +3,19 @@
 # Copyright (C) 2019 tribe29 GmbH - License: Checkmk Enterprise License
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
+
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict
 
-from .bakery_api.v1 import FileGenerator, OS, Plugin, register
+from .bakery_api.v1 import FileGenerator, OS, Plugin, PluginConfig, register
 
-
-def get_fail2ban_files(conf: Any) -> FileGenerator:
-    yield Plugin(base_os=OS.LINUX, source=Path("fail2ban"))
-
+def get_fail2ban_files(conf: Dict[str, Any]) -> FileGenerator:
+    interval = conf.get('interval')
+    yield Plugin(base_os=OS.LINUX,
+                 source=Path("fail2ban"),
+                 interval=interval)
 
 register.bakery_plugin(
     name="fail2ban",
-    files_function=fail2ban,
+    files_function=get_fail2ban_files,
 )
-
-
